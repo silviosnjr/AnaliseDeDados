2334bd4ffba8ede9c73da2e992e4b78814ba886b
Silvio Sales do Nascimento Junior <silviosnjr@gmail.com>
2024-10-07 17:24:01 -0300

    Add files via upload

---
+++ graficos/quantidadeUsuarios.js
@@ -0,0 +1,33 @@
+import { getCSS } from "./common.js"
+
+async function quantidadeUsuarios() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const rest = await fetch(url)
+    const dados = await rest.json()
+
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeUsuarios= Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes,
+            y: quantidadeUsuarios,
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color')
+    }
+    
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, layout)
+}
+ 
+quantidadeUsuarios()
